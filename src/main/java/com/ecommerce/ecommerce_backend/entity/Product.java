b64333eb1fd71f791e8b7c4cd67dcf3b4dd07ebe
saichinnam1 <saichinnam566@gmail.com>
2025-05-07 21:10:09 +0530

    updated product entity

--- src/main/java/com/ecommerce/ecommerce_backend/entity/Product.java
+++ src/main/java/com/ecommerce/ecommerce_backend/entity/Product.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 
-
+@Entity
 public class Product {
     private Long id;
     private String name;
